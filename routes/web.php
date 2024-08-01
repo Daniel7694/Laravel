56f2506b1113854bf72b1ff757a11e75f9bae966
Daniel7694 <107689067+Daniel7694@users.noreply.github.com>
2024-08-01 15:09:44 -0500

    routes

--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,16 @@ Route::get('/post', function () {
     return "Aqui se mostraran todos los posts";
 });
 
-Route::get('/post/{post}/{category?}', function ($post, $category = null) {
+Route::get('/post/create', function () {
 
-    if ($category)
-    {    return "Aqui se mostraran todos los {$post}, con la categoria {$category}";
-     } else {
-        return "Aqui se mostraran todos los {$post}";
-     }
+    return "Aqui se mostrara el formulario para crear un nuevo post";
+});
 
-
+Route::get('/post/{post}', function ($post) {
+    return "Aqui se mostraran todos los {$post}";
 });
 
 
-/* Route::get('/post/{post}/{category}', function ($post, $category) {
+/*Route::get('/post/{post}/{category}', function ($post, $category) {
     return "Aqui se mostraran todos los {$post}, con la categoria {$category}";
-}); */
+});*/
