dad7debcfd8830d26be114ca09eb8974dc7d53ea
jeam.nieto769@gmail.com <107689067+Daniel7694@users.noreply.github.com>
2024-08-05 11:02:18 -0500

    Components

---
+++ resources/views/components/alert.blade.php
@@ -0,0 +1,9 @@
+@props(['type' => 'info'])
+
+@php
+   
+@endphp
+
+<div {{$attributes->merge(['class' => 'p-4 text-sm rounded-lg ' . $class])}} role="alert">
+  <span class="font-medium">{{ $title ?? 'Info alert' }}</span> {{ $slot }}
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel 11</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <h1>Welcome to the homepage</h1>
+<div class="max-w-4xl mx-auto px-4">
+   {{--<h1>Welcome to the homepage</h1>--}} 
+
+    <x-alert2 type="info" class="mb-4">
+       <x-slot name="title">
+            Este es el titulo de la alerta
+        </x-slot>
+
+        contenido de la alerta 
+    </x-alert2>
+
+    <p>Hola mundo</p>
+</div>
 </body>
 </html>
