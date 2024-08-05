78ccb911653a93247a4487360a85193d7060db6a
jeam.nieto769@gmail.com <107689067+Daniel7694@users.noreply.github.com>
2024-08-05 13:01:44 -0500

    Eloquent

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
+use App\Models\Post;
+
 //con __invoke, un solo metodo
 Route::get('/', [HomeController::class, 'index'] );
 
@@ -16,3 +18,48 @@ Route::get('/post/{post}', [PostController::class, 'show'] );
 /*Route::get('/post/{post}/{category}', function ($post, $category) {
     return "Aqui se mostraran todos los {$post}, con la categoria {$category}";
 });*/
+
+Route::get('prueba', function(){
+    /*
+    Crear nuevo post
+
+  $post = new Post;
+
+  $post->title = 'Título prueba 3';
+  $post->content = 'Contenido prueba 3';
+  $post->categorias = 'Categoria prueba 3';
+
+  $post->save();
+
+  return $post;
+  */
+
+  /* $post = Post::find(1); */
+
+/*
+    Actualizar registro
+  $post = Post::where('title', 'Título prueba 2')
+        ->first();
+  $post->categorias = 'Desarrollo web';
+  $post->save();
+
+  return $post;
+  */
+/*
+Listar post
+  $post = Post::orderBy('categorias', 'ASC')
+                ->take(2)
+                ->get();
+
+  return $post; */
+
+
+  
+  //Eliminar registro
+
+  $post = Post::find(2);
+  $post->delete();
+
+  return $post;
+
+});
