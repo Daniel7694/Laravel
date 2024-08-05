2f94a6edb48f83ef184038bd580ef88f95ec84ce
jeam.nieto769@gmail.com <107689067+Daniel7694@users.noreply.github.com>
2024-08-05 11:23:44 -0500

    Layouts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel 11</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-<body>
+
+@extends('layouts.app')
+
+@section('title', 'laravel')
+
+@push('css')
+    <style>
+        body {
+            background-color: #f1f1f1;
+        }
+    </style>
+@endpush
+
+@push('css')
+    <style>
+        body {
+            color: #ff0000;
+        }
+    </style>
+@endpush
+
+@section('content')
 <div class="max-w-4xl mx-auto px-4">
-   {{--<h1>Welcome to the homepage</h1>--}} 
+    {{--<h1>Welcome to the homepage</h1>--}} 
+ 
+     <x-alert2 type="info" class="mb-4">
+        <x-slot name="title">
+             Este es el titulo de la alerta
+         </x-slot>
+ 
+         contenido de la alerta 
+     </x-alert2>
+ 
+     <p>Hola mundo</p>
+ 
+@endsection
 
-    <x-alert2 type="info" class="mb-4">
-       <x-slot name="title">
-            Este es el titulo de la alerta
-        </x-slot>
 
-        contenido de la alerta 
-    </x-alert2>
-
-    <p>Hola mundo</p>
-</div>
-</body>
-</html>
+   
